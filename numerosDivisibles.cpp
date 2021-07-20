174b65662f72db63641ed298a7ef7abfd783e80b
Claudia <claudiatcruz2002@gmail.com>
2021-07-20 18:08:47 +0100

    codigo hecho v1

--- numerosDivisibles.cpp
+++ numerosDivisibles.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 //------------------------------------------------------------------------------------------------------------------
+//funcion
+void numerosDivisibles(int *original, int tamO, int *divisores, int tamD)
+{
+    for (int i = 0; i < tamD; i++)
+    {
+        for (int j = 0; j < tamO; j++)
+        {
+            if (original[j] % divisores[i] == 0)
+            {
+                std::cout << " " << original[j] << "\n";
+            }
+        }
+    }
+}
+//------------------------------------------------------------------------------------------------------------------
+int main(int argc, char const *argv[])
+{
+    int listaOriginal[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int listaDivisores[] = {2, 3, 4};
+    numerosDivisibles(&listaOriginal[0], 8, &listaDivisores[0], 3);
+    return 0;
+}
