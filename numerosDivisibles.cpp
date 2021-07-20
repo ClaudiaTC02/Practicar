40f814e0c4a72b53a189344a388db2b7d1e0624c
Claudia <claudiatcruz2002@gmail.com>
2021-07-20 18:40:04 +0100

    programa hecho sin arrays individuales

--- numerosDivisibles.cpp
+++ numerosDivisibles.cpp
@@ -5,17 +5,18 @@ void numerosDivisibles(int *original, int tamO, int *divisores, int tamD)
 {
     for (int i = 0; i < tamD; i++)
     {
+        std::cout << "Divisible por " << divisores[i] << " : " << "\n";
         for (int j = 0; j < tamO; j++)
         {
             if (original[j] % divisores[i] == 0)
             {
-                std::cout << " " << original[j] << "\n";
+                std::cout << original[j] << "\n";
             }
         }
     }
 }
 //------------------------------------------------------------------------------------------------------------------
-int main(int argc, char const *argv[])
+int main()
 {
     int listaOriginal[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int listaDivisores[] = {2, 3, 4};
