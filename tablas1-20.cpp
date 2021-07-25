52193155a88e2a9edd971e6af4d59b61e57d9fe0
Claudia <claudiatcruz2002@gmail.com>
2021-07-25 21:14:49 +0100

    codigo tablas1-20 v1

--- tablas1-20.cpp
+++ tablas1-20.cpp
@@ -1,29 +1,28 @@
+
 #include <iostream>
 //------------------------------------------------------------------------------------------------------------------
 //funcion
-void tablas(int *resultados, int &contador){
-    int listas[1000];
-    int tam = 0;
-    for (int i = 0; i < 4; i++)
-    {   //3
+void tablas(int *resultados, int &contador)
+{
+    int producto = 0;
+    for (int i = 0; i < 5; i++)
+    { //3
         for (int j = 0; j < 11; j++)
-        {   //5
-            tam++;
-            std::cout<<i<<" x "<<j<< " = ";
-            for (int k = 0; k < tam; k++)
+        { //5
+            producto = i*j;
+            resultados[contador++] = producto;
+            for (int k = 0; k < contador; k++)
             {
-                if (resultados[k] != i*j)
+                if (resultados[k - 1] == producto)
                 {
-                    resultados[contador++] = i*j;
-                } 
+                    std::cout<<"Hola "<<producto<<"\n";
+                    contador--;
+                    break;
+                }
             }
-
         }
     }
 }
-
-
-
 
 //------------------------------------------------------------------------------------------------------------------
 int main()
@@ -31,10 +30,10 @@ int main()
     int lista[1000];
     int contador = 0;
     tablas(&lista[0], contador);
-    std::cout<<"el contador: "<<contador<<"\n";
+    std::cout << "el contador: " << contador << "\n";
     for (int i = 0; i < contador; i++)
     {
-        std::cout<<lista[i]<<"\n";
+        std::cout << lista[i] << "\n";
     }
     return 0;
 }
